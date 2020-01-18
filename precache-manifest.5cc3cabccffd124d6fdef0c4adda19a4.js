680e362210c9b7fe82e4a7e01a247f0c4da7ed7a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-01-18 13:30:22 +0000

    Deploy sanjeet05/portfolio to github.com/sanjeet05/portfolio.git:gh-pages

---
+++ precache-manifest.5cc3cabccffd124d6fdef0c4adda19a4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5e6ecb90385fbaec3286",
+    "url": "/static/css/main.c853c57b.chunk.css"
+  },
+  {
+    "revision": "5e6ecb90385fbaec3286",
+    "url": "/static/js/main.5e6ecb90.chunk.js"
+  },
+  {
+    "revision": "f355a0ed61ad2d440831",
+    "url": "/static/js/1.f355a0ed.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e9c9ee1c5fa7bbafcfac6f26cae8c43a",
+    "url": "/static/media/sanjeet_img.e9c9ee1c.jpg"
+  },
+  {
+    "revision": "33b46d75ed214bbc359359772ce77186",
+    "url": "/static/media/sanjeet_qrcode.33b46d75.png"
+  },
+  {
+    "revision": "90da0fd0cbe5d4ba3900f893b7acb9d7",
+    "url": "/index.html"
+  }
+];
