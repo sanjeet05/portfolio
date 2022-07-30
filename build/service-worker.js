dff1eee226a30393424b3554af0b13450c16081c
sanjeet <sanjeet.kumar@twinhealth.com>
2022-07-30 14:54:58 +0530

    updated

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.86fdbe12ce43e3c7e64f8c868d99d489.js"
+  "/precache-manifest.9cc2b6ce8bc67311f24369acfb20b25c.js"
 );
 
 workbox.clientsClaim();
